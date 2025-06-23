e23c5f2f3ff43a2e4ea3816929403da0e09f7b83
anna <luizafr62@gmail.com>
2025-06-22 23:14:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector("#mensagemBotao").addEventListener("click", function() {
+    document.querySelector("#mensagem").textContent = "“O fim não é um ponto final. É um ponto de reconexão.”"
+});
